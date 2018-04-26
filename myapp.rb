240402e8c7912de563f3a1e0808662fa7d012091
at-laibui <lai.bui@asiantech.vn>
2018-04-26 16:23:44 +0700

    add list

--- myapp.rb
+++ myapp.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'pry'
 require 'mysql2'
-client = Mysql2::Client.new(
+@@client = Mysql2::Client.new(
   host: 'localhost',
   username: 'root',
   password: 'Abc123@@'
 )
-client.query('use football')
+@@client.query('use football')
 # get '/' do
 #   'hello world'
 # end
@@ -14,36 +14,39 @@ client.query('use football')
 # #   binding.pry
 # #   "hello #{params['name']}!"
 # # end
+def output(str)
+  arr = @@client.query('select * from football_name')
+  a = str
+  a << " \n danh sach dang co \n"
+  arr.each do |row|
+    a << "#{row['indexID']} : #{row['name']} \n"
+  end
+  a
+end
 get '/hello/:name' do |n|
   "hello #{n}!"
 end
 post '/posts' do
   id = params['indexID']
   name = params['name']
-  client.query("
-  INSERT INTO football_name(indexID,name)
-  VALUES (#{id},'#{name}')")
-  arr = client.query('select * from football_name')
-  a = " Da them #{id} của #{name} \n danh sach dang co \n"
-  arr.each do |row|
-    a << ''
-    a << "#{row['indexID']} : #{row['name']} \n"
-  end
-  a
+  @@client.query("
+    INSERT INTO football_name(indexID,name)
+    VALUES (#{id},'#{name}')")
+  output('Da Tao')
 end
 delete '/delete/:id' do |id|
-  client.query(
+  @@client.query(
     "DELETE FROM football_name where indexID = #{id}"
   )
-  'deleted'
+  output('Da Xoa')
 end
 put '/put/:id' do |id|
   ids = params['indexID']
   name = params['name']
-  client.query(
+  @@client.query(
     "UPDATE football_name
     SET indexID = #{ids}, name = '#{name}'
     where indexID = #{id}"
   )
-  'updated'
+  output('Da Sua')
 end
